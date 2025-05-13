32db2732f69655642063ee3c85ed4e5c86135488
jinwoo seo <sjinwoo0612@gmail.com>
2025-05-13 20:06:05 +0900

    Update encoder.py

--- videollama2/model/encoder.py
+++ videollama2/model/encoder.py
@@ -93,7 +93,7 @@ class SiglipVisionTower(nn.Module):
         self.image_processor = SiglipImageProcessor.from_pretrained(self.vision_tower_name)
 
         config = SiglipVisionConfig.from_pretrained(self.vision_tower_name)
-        config._attn_implementation = 'flash_attention_2'
+        config._attn_implementation = 'eager'
 
         if not load_pretrained:
             self.vision_tower = SiglipVisionModel(config=config)
